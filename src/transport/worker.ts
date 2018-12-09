6b152e84b22cdbb3d01377c45b9f128d8567f8f5
carney520 <carney520@hotmail.com>
2018-12-09 18:46:51 +0800

    refactor workersource, make it testable.

--- src/transport/worker.ts
+++ src/transport/worker.ts
@@ -21,6 +21,8 @@ interface PeerInitialState {
   name: string
 }
 
+type ExtendedPort = MessagePort & { zoombie: boolean; id: number; name: string }
+
 const WorkerPeer = {
   id: -1,
   name: 'worker',
@@ -183,7 +185,13 @@ export default class WorkerTransport extends EventEmitter implements Transport {
   }
 
   private genSource() {
-    const source = `(${workerSource.toString()})(${JSON.stringify(EVENTS)})`
+    const source = `
+    (function(window){
+      var ITCWorker = (${workerSource.toString()})(${JSON.stringify(EVENTS)});
+      new ITCWorker(window);
+    })(this)
+    `
+
     const sourceHash = hash(source)
     const key = `itc-sw-${sourceHash}`
     let cachedUrl = window.localStorage.getItem(key)
@@ -200,140 +208,148 @@ export default class WorkerTransport extends EventEmitter implements Transport {
 /**
  * worker 源代码
  */
-export function workerSource(this: SharedWorker.SharedWorkerGlobalScope, events: typeof EVENTS) {
-  type ExtendedPort = MessagePort & { zoombie: boolean; id: number; name: string }
-  const ports: ExtendedPort[] = []
-  let master: ExtendedPort | undefined
-  let uid: number = 0
+export function workerSource(events: typeof EVENTS) {
+  return class {
+    ports: ExtendedPort[] = []
+    master: ExtendedPort | undefined
+    scope: SharedWorker.SharedWorkerGlobalScope
+    uid: number = 0
 
-  function checkMaster() {
-    if (master == null && ports.length) {
-      master = ports[0]
-      master.postMessage({ type: events.BECOME_MASTER })
-      broadcast({ type: events.UPDATE_MASTER, data: { id: master.id, name: master.name } })
-    }
-  }
-
-  function removePort(port: ExtendedPort) {
-    const index = ports.indexOf(port)
-    if (index !== -1) {
-      ports.splice(index, 1)
+    checkMaster() {
+      if (this.master == null && this.ports.length) {
+        this.master = this.ports[0]
+        this.master.postMessage({ type: events.BECOME_MASTER })
+        this.broadcast({ type: events.UPDATE_MASTER, data: { id: this.master.id, name: this.master.name } })
+      }
     }
 
-    if (master === port) {
-      master = undefined
+    removePort(port: ExtendedPort) {
+      const index = this.ports.indexOf(port)
+      if (index !== -1) {
+        this.ports.splice(index, 1)
+      }
+
+      if (this.master === port) {
+        this.master = undefined
+      }
+
+      this.updatePeer()
+      this.checkMaster()
     }
 
-    updatePeer()
-    checkMaster()
-  }
-
-  /**
-   * sync peers
-   */
-  function updatePeer() {
-    ports.forEach(port => {
-      const peers = getPeers(port)
-      port.postMessage({ type: events.UPDATE_PEERS, data: peers })
-    })
-  }
-
-  function getPeers(port: ExtendedPort) {
-    return ports.filter(p => p.id !== port.id).map(p => ({ id: p.id, name: p.name }))
-  }
-
-  function broadcast(data: MesssagePayload) {
-    ports.forEach(port => port.postMessage(data))
-  }
-
-  function postMessage(data: WorkerPayload) {
-    if (data.target == null || data.target === '*') {
-      broadcast(data)
-      return
+    getPeers(port: ExtendedPort) {
+      return this.ports.filter(p => p.id !== port.id).map(p => ({ id: p.id, name: p.name }))
     }
 
-    if (data.target === -1) {
-      return
+    /**
+     * sync peers
+     */
+    updatePeer() {
+      this.ports.forEach(port => {
+        const peers = this.getPeers(port)
+        port.postMessage({ type: events.UPDATE_PEERS, data: peers })
+      })
     }
 
-    const idx = ports.findIndex(i => i.id === data.target)
-    if (idx !== -1) {
-      ports[idx].postMessage(data)
+    broadcast(data: MesssagePayload) {
+      this.ports.forEach(port => port.postMessage(data))
     }
-  }
 
-  function heartbeat() {
-    setTimeout(() => {
-      let i = ports.length
-      while (i--) {
-        const port = ports[i]
-        if (port.zoombie) {
-          removePort(port)
-        } else {
-          port.zoombie = true
-          port.postMessage({ type: events.PING })
+    postMessage(data: WorkerPayload) {
+      if (data.target == null || data.target === '*') {
+        this.broadcast(data)
+        return
+      }
+
+      if (data.target === -1) {
+        return
+      }
+
+      const idx = this.ports.findIndex(i => i.id === data.target)
+      if (idx !== -1) {
+        this.ports[idx].postMessage(data)
+      }
+    }
+
+    heartbeat() {
+      setTimeout(() => {
+        let i = this.ports.length
+        while (i--) {
+          const port = this.ports[i]
+          if (port.zoombie) {
+            this.removePort(port)
+          } else {
+            port.zoombie = true
+            port.postMessage({ type: events.PING })
+          }
         }
-      }
-      heartbeat()
-    }, 500)
-  }
-
-  this.addEventListener('connect', function(event: Event) {
-    const port = (event as MessageEvent).ports[0] as ExtendedPort
-    port.id = uid++
-
-    port.addEventListener('message', function(evt: MessageEvent) {
-      // reconnect
-      if (ports.indexOf(port) === -1) {
-        ports.push(port)
-        checkMaster()
-        updatePeer()
-        // force update master
-        postMessage({
-          target: port.id,
-          type: events.UPDATE_MASTER,
-          data: { id: master!.id, name: master!.name },
-        } as WorkerPayload)
-      }
-
-      const message = evt.data as WorkerPayload
-      switch (message.type) {
-        case events.PONG:
-          port.zoombie = false
-          break
-        case events.MESSAGE:
-          // forward to other ports
-          postMessage(message)
-          break
-        case events.DESTORY:
-          removePort(port)
-          break
-        case events.INITIAL:
-          const { name } = message.data as PeerInitialState
-          port.name = name
-          updatePeer()
-          break
-        default:
-          // forward to other ports
-          postMessage(message)
-          break
-      }
-    })
-
-    ports.push(port)
-    port.start()
-    const currentMaster = master || port
-    const initialState: InitializeState = {
-      id: port.id,
-      peers: getPeers(port),
-      master: { name: currentMaster.name, id: currentMaster.id },
+        this.heartbeat()
+      }, 500)
     }
-    port.postMessage({
-      type: events.CONNECTED,
-      data: initialState,
-    })
-    checkMaster()
-  })
 
-  heartbeat()
+    constructor(scope: SharedWorker.SharedWorkerGlobalScope) {
+      this.scope = scope
+      this.listen()
+      this.heartbeat()
+    }
+
+    listen() {
+      this.scope.addEventListener('connect', (event: Event) => {
+        const port = (event as MessageEvent).ports[0] as ExtendedPort
+        port.id = this.uid++
+
+        port.addEventListener('message', (evt: MessageEvent) => {
+          // reconnect
+          if (this.ports.indexOf(port) === -1) {
+            this.ports.push(port)
+            this.checkMaster()
+            this.updatePeer()
+            // force update master
+            this.postMessage({
+              target: port.id,
+              type: events.UPDATE_MASTER,
+              data: { id: this.master!.id, name: this.master!.name },
+            } as WorkerPayload)
+          }
+
+          const message = evt.data as WorkerPayload
+          switch (message.type) {
+            case events.PONG:
+              port.zoombie = false
+              break
+            case events.MESSAGE:
+              // forward to other ports
+              postMessage(message)
+              break
+            case events.DESTORY:
+              this.removePort(port)
+              break
+            case events.INITIAL:
+              const { name } = message.data as PeerInitialState
+              port.name = name
+              this.updatePeer()
+              break
+            default:
+              // forward to other ports
+              this.postMessage(message)
+              break
+          }
+        })
+
+        this.ports.push(port)
+        port.start()
+        const currentMaster = this.master || port
+        const initialState: InitializeState = {
+          id: port.id,
+          peers: this.getPeers(port),
+          master: { name: currentMaster.name, id: currentMaster.id },
+        }
+        port.postMessage({
+          type: events.CONNECTED,
+          data: initialState,
+        })
+        this.checkMaster()
+      })
+    }
+  }
 }
