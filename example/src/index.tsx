f7d2e81aa9c597bb3dfd4f20107341d3959bbc6a
carney520 <carney520@hotmail.com>
2018-12-13 18:36:45 +0800

    example update

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,18 +1,36 @@
 /**
  * App entry
  */
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 import './style.css'
 
-const peers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+const peers = ['A', 'B', 'C', 'D']
+
 function App() {
+  const [sleeping, setSleeping] = useState<{ [key: string]: boolean | undefined }>({})
   return (
     <div className="container">
-      {peers.map(p => (
-        <iframe key={p} className="peer-container" src={`peer.html?name=${p}`} />
-      ))}
+      {peers.map(p => {
+        const isSleeping = sleeping[p]
+        return (
+          <div className="peer-container" key={p}>
+            {isSleeping ? <div className="placeholder">sleeping</div> : <iframe src={`peer.html?name=${p}`} />}
+            <div>
+              <button
+                // tslint:disable-next-line:jsx-no-lambda
+                onClick={() => {
+                  sleeping[p] = !sleeping[p]
+                  setSleeping(sleeping)
+                }}
+              >
+                {isSleeping ? 'wake' : 'sleep'}
+              </button>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- example/src/peer.tsx
+++ example/src/peer.tsx
@@ -1,8 +1,8 @@
 /**
  * App entry
  */
-import itc, { Transport } from '@carney520/itc'
-import React, { useEffect, useRef, useState } from 'react'
+import itc, { Transport, Peer } from '@carney520/itc'
+import React, { useEffect, useRef, useState, useCallback } from 'react'
 import ReactDOM from 'react-dom'
 
 import './style.css'
@@ -10,6 +10,17 @@ import './style.css'
 function App() {
   const ref = useRef<{ worker?: Transport }>({})
   const [isMaster, setIsMaster] = useState(false)
+  const [master, setMaster] = useState<Peer | undefined>(undefined)
+  const [peers, setPeers] = useState<Peer[]>([])
+  const [position, setPosition] = useState<[number, number]>([0, 0])
+  const handleMouseMove = useCallback((evt: React.MouseEvent) => {
+    const pos: [number, number] = [evt.clientX, evt.clientY]
+    setPosition(pos)
+    if (ref.current.worker) {
+      ref.current.worker.send(pos)
+    }
+  }, [])
+
   useEffect(() => {
     const name = location.search.match(/name=(.*)$/)![1]
     const worker = (ref.current.worker = itc(name, { useStorage: true }))
@@ -19,9 +30,24 @@ function App() {
     worker.on('masterlose', () => {
       setIsMaster(false)
     })
+    worker.on('masterupdate', m => {
+      setMaster(m)
+    })
+    worker.on('peerupdate', p => {
+      setPeers(p)
+    })
+    worker.on('message', (data: [number, number]) => {
+      setPosition(data)
+    })
   }, [])
 
-  return <div>isMaster: {JSON.stringify(isMaster)}</div>
+  return (
+    <div className={`${isMaster ? 'master' : ''} peer`} onMouseMove={handleMouseMove}>
+      <div>current master: {master && master.name}</div>
+      <div>peers: [{peers.map(i => i.name).join(', ')}]</div>
+      <div className="ball" style={{ left: position[0], top: position[1] }} />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
